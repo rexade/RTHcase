d764883f942700e74e89b0dc0d20697ecd0826d2
mypav1 <robin.magnusson@gmail.com>
2019-11-21 14:39:37 +0100

    Added some more elements
    More elements added

--- IKEA/MySeleniumProject/PageElements.cs
+++ IKEA/MySeleniumProject/PageElements.cs
@@ -20,11 +20,25 @@ namespace LandindPageClass {
 
         }
 
+
+        /* STARTSIDAN
+
+        */
         public IWebElement CookieBannerButton() //Cookie accept button
         {
             return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("onetrust-accept-btn-handler")));
         }
 
+        public IWebElement ShoppingBag() //Klicka på kundvagn från startsidan
+        {
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/header/div[2]/div/nav[2]/ul/li[3]/a/span[2]")));
+        }
+
+
+
+        /*INKÖPSLISTAN
+         
+        */
         public IWebElement BuyList() //Klicka på inköpslistan
         {
             return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/header/div[2]/div/nav[2]/ul/li[2]/a")));
@@ -38,6 +52,29 @@ namespace LandindPageClass {
         public IWebElement Products() //Klicka på "bläddra bland våra produkter" inifrån inköpslistan
         {
             return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"one-checkout\"]/div[2]/div/div[3]/div[2]/div[2]/div[1]/a")));
+        }
+
+        public IWebElement LogInButtonBuyList() //Klicka på login knappen inifrån inköpslistan
+        {
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"one-checkout\"]/div[2]/div/div[3]/div[2]/div[2]/div[2]")));
+        }
+
+        public IWebElement CreateAccountBuyList() //Klicka på skapa ett konto knappen inifrån inköpslistan
+        {
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"one-checkout\"]/div[2]/div/div[3]/div[2]/div[1]/a")));
+        }
+
+        /*KUNDVAGN
+         
+        */
+        public IWebElement AddProductArtNr() //Lägg till vara via artikelnummer 
+        {
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"one-checkout\"]/div[2]/div/div[2]/div[1]/div/button/span")));
+        }
+
+        public IWebElement AddProductArtNrButton() //Klicka på lägg till vara via artikelnummer 
+        {
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"one-checkout\"]/div[2]/div/div[2]/div[1]/form/div[2]/button/span")));
         }
 
 
